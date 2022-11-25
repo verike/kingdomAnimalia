fcdb35a392ea557573aaa31bdb283d0d856a4b24
Victor Erike <verike7@gmail.com>
2022-11-25 14:00:57 +0100

    Initial commit

---
+++ cinema.js
@@ -0,0 +1,18 @@
+// OUTPUT "How old are you?"
+// INPUT take user input
+// STORE users input in age
+
+let age = prompt("How old are you? :");
+
+// IF age < 15
+if (age < 15)
+{
+    // OUTPUT you are eligible for a discount
+    console.log("You are eligible for a discount");
+}
+// ELSE
+else
+{
+    // OUTPUT you are not eligble for the discount
+    console.log("You are not eligible for the discount");
+}
